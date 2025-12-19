12ca85ffac5a8ba1b63706eadcbe8afe8eb80dee
vbikman <vbikman@mindk.com>
2025-12-19 19:35:56 +0200

    AI ver 1.0.1: fixed header

--- projects/apps/user/src/app/app.component.ts
+++ projects/apps/user/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
 })
 export class AppComponent {
   title = 'user';
